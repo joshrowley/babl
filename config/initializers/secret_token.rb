91553933e62541cb096e18870564f67de4d5b270
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-10 01:50:20 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babl::Application.config.secret_token = '7c29623335d0441d30c8896b59c39b518a979a619f7d7f9c110811d8d6c3a4d9c1ae6f9be44f244897a917524a5ea86597a8c312ff7c87d0fbd55a20c304e599'
