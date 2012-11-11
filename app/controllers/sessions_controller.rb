344f2c8e7e982667a06fc6e65e0f17526c4296a1
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-11 12:28:51 -0500

    notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,20 @@ class SessionsController < ApplicationController
 
   def create
 
-    render :text => request.env['omniauth.auth'].inspect.to_yaml
+    # render :text => request.env['omniauth.auth'].inspect.to_yaml
+
+    auth_hash = request.env['omniauth.auth']
+
+    @authorization = Authorization.find_by_provider_and_uid(auth_hash.info.provider, auth_hash.uid)
+
+    if @authorization
+      render :text => "Welcome back #{@authorization.user.name}! You're logged in through #{@authorization.provider}."
+    else
+      user = User.new(:name => auth_hash.info.name, :email => auth_hash.info.email)
+      user.authorizations.build(:provider => auth_hash.provider, :uid => auth_hash.uid)
+      user.save
+      render :text => "Hi #{user.name}! Your account has been signed up."
+    end
 
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :email
 
   has_many :authorizations
 end
