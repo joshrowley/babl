cd12ef9b6ae54cbc9a43b6a7ae8d8a428ed87946
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-10 21:26:44 -0500

    add email attr to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121111011226) do
+ActiveRecord::Schema.define(:version => 20121111022546) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20121111011226) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
 end
