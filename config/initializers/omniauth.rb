e92e88393496577605ebd6a58406bd4a8c161d93
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-14 19:22:31 -0500

    remove omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-# 3.1.3
-Rails.application.config.middleware.use OmniAuth::Builder do
-      
-  provider :facebook, '305526829561222', '53985e48bc5b0c0318d1567763477f9b'
-
-  # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  # provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-
-end
